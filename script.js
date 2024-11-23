9f301f8fc5097769c95089cefefc19d8ae181494
Yonas-gen <yonasfirew123456+1@gmail.com>
2024-11-23 12:57:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function changebg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+
+
+    banner.style.background = `url("images/n/${bg}")`;
+    // banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center'; 
+
+
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+}
